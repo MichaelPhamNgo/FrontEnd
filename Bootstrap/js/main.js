d3947a2d53a3a9cd600c6839492a23a3cc9a885a
Michael Pham <michaelphamn@gmail.com>
2019-10-12 11:43:27 -0700

    Work on the headers

---
+++ Bootstrap/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#button-setting").click(function(){
+        $("#nav-setting-header-0").toggle();
+    });
+    $("#button-currency").click(function(){
+        $("#nav-currency-header-0").toggle();
+    });
+    $("#button-language").click(function(){
+        $("#nav-language-header-0").toggle();
+    });
+});
